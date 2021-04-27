aaa7ebf96f5c3d96da75e438f4885ce573d2eda7
Akrem <Akremabdu71@gmail.com>
2021-04-26 21:46:33 -0500

    we made

--- src/classa/test.java
+++ src/classa/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("thank you ");
+        System.out.println("Andreea is shy ");
     }
 }
